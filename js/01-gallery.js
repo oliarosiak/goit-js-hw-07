8acbcf4dfcd0baba539863f627f7e5947e5b1f12
Olia Rosiak <olia.rosiak@gmail.com>
2022-01-21 19:38:46 +0200

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -24,16 +24,72 @@ import { galleryItems } from './gallery-items.js';
   </a>
 </div> */
 
-const galleryList = document.querySelector('div.gallery');
+/**
+ * Варіант 1
+*/
 
-const markupGallery = galleryItems.map(createGalleryItems).join('');
-galleryList.insertAdjacentHTML('afterbegin', markupGallery);
+// const galleryContainer = document.querySelector('div.gallery');
 
-galleryList.addEventListener('click', callBigImage);
+// const markupGallery = galleryItems.map(createGalleryItems).join('');
+// galleryContainer.insertAdjacentHTML('afterbegin', markupGallery);
 
-function createGalleryItems({ description, preview, original }) {
-    return `
-    <div class="gallery__item">
+// galleryContainer.addEventListener('click', callBigImage);
+
+// function createGalleryItems({ description, preview, original }) {
+//     return `
+//     <div class="gallery__item">
+//         <a class="gallery__link" href="${original}">
+//             <img
+//             class="gallery__image"
+//             src="${preview}"
+//             data-source="${original}"
+//             alt="${description}"
+//             />
+//         </a>
+//     </div>
+//     `;
+// }
+
+// function callBigImage(event) {
+//   event.preventDefault();
+//   const bigImageURL = event.target.dataset.source;     
+
+//   //Перевірка може бути ще така: event.currentTarget === event.target
+//   if (event.target.nodeName !==`IMG`) {
+//     console.log('Фуууу, мимо');
+//     return;
+//   }
+    
+//   const bigImage = basicLightbox.create(`
+//     <img src="${bigImageURL}" width="800" height="600">
+//   `);   
+//   bigImage.show();
+//   console.log('Молодєц, попала');
+
+//   window.addEventListener('keydown', escapeClick);
+
+//   function escapeClick(event) {
+//     console.log('code', event.code);
+//     if (event.code === "Escape") {
+//       bigImage.close();
+//       window.removeEventListener('keydown', escapeClick);
+//       console.log('fooooo', event.code);
+//     }
+//   }  
+// };
+
+
+/**
+ * Варіант 2
+*/
+
+const galleryDiv = document.querySelector('div.gallery');
+
+const markupGalleryItem = galleryItems.map(item => { 
+  const { description, preview, original } = item;
+
+  return `
+  <div class="gallery__item">
         <a class="gallery__link" href="${original}">
             <img
             class="gallery__image"
@@ -43,26 +99,135 @@ function createGalleryItems({ description, preview, original }) {
             />
         </a>
     </div>
-    `;
+  `
+}).join('');
+
+galleryDiv.insertAdjacentHTML('afterbegin', markupGalleryItem);
+galleryDiv.addEventListener('click', getBigImageUrl);
+
+// let imgShow;
+
+function getBigImageUrl(event) {
+  event.preventDefault();
+
+  const bigImageUrl = event.target.dataset.source;
+  const bigImageAlt = event.target.alt;
+
+  // if (event.target.nodeName !== 'IMG') {
+  //   console.log('Мазіла');
+  //   return;
+  // }
+
+  document.querySelector('a.gallery__link').onclick = () => {
+    const bigImg = `<img src="${bigImageUrl}" alt="${bigImageAlt}" width="800" height="600">`;
+
+    const imgShow = basicLightbox.create(bigImg, {
+      onShow: (bigImg) => console.log('onShow', bigImg),
+      onClose: (bigImg) => console.log('onClose', bigImg)
+    });
+
+    imgShow.show(() => window.addEventListener('keydown', checkEscapeBtnClick));
+
+    function checkEscapeBtnClick(event) {
+      const ESC_KEY_CODE = 'Escape';
+      const isEscapeKey = event.code === ESC_KEY_CODE;
+      console.log('відкрито', event.code);
+
+      if (isEscapeKey) {
+        imgShow.close(() => {
+          window.removeEventListener('keydown', checkEscapeBtnClick);
+          console.log('закрито', event.code);
+        })
+      }
+    }  
+  }
+  //console.log('Мєткий глаз');
+
+  // const bigImg = `<img src="${bigImageUrl}" alt="${bigImageAlt}" width="800" height="600">`;
+
+  // const imgShow = basicLightbox.create(bigImg, {
+  //   onShow: (bigImg) => console.log('onShow', bigImg),
+  //   onClose: (bigImg) => console.log('onClose', bigImg)
+  // });
+
+  // imgShow.show(() => window.addEventListener('keydown', checkEscapeBtnClick));
+
+  // function checkEscapeBtnClick(event) {
+  //   const ESC_KEY_CODE = 'Escape';
+  //   const isEscapeKey = event.code === ESC_KEY_CODE;
+  //   console.log('відкрито', event.code);
+
+  //   if (isEscapeKey) {
+  //     imgShow.close(() => {
+  //       window.removeEventListener('keydown', checkEscapeBtnClick);
+  //       console.log('закрито', event.code);
+  //     })
+  //   }
+  // }
+
+
+  /** const content = document.createElement('div')
+
+		const h1 = document.createElement('h1')
+		const p = document.createElement('p')
+
+		h1.textContent = 'Create element'
+		p.textContent = 'Create elements dynamicly and use them in your lightbox.'
+
+		content.appendChild(h1)
+		content.appendChild(p)
+
+		const instance = basicLightbox.create(content)
+
+		document.querySelector('button.create').onclick = instance.show
+ */
+  
+  
+
+  // imgShow = basicLightbox.create(`<img src="${bigImageUrl}" alt="${bigImageAlt}" width="800" height="600">`);
+  // document.querySelector('a.gallery__link').onclick = imgShow.show();
+  
+  //imgShow.show(() => window.addEventListener('keydown', checkEscapeBtnClick));  
+
+  /**
+   *document.querySelector('button.image').onclick = () => {
+			basicLightbox.create(`
+				<img width="1400" height="900" src="https://placehold.it/1400x900">
+			`).show()
+		}
+
+    document.querySelector('button.callbacks').onclick = (e) => {
+
+			const html = `
+				<h1>Callbacks</h1>
+				<p>Take a look at the console of your browser.<br>This lightbox will close automaticly to demonstrate the close-callback.</p>
+			`
+
+			const instance = basicLightbox.create(html, {
+				onShow: (instance) => console.log('onShow', instance),
+				onClose: (instance) => console.log('onClose', instance)
+			})
+
+			instance.show((instance) => console.log('finished show()', instance))
+
+			setTimeout(() => {
+				instance.close((instance) => console.log('finished close()', instance))
+			}, 3000)
+
+		}
+   */
 }
 
-function callBigImage(event) {
-    event.preventDefault();
+/** */
+// function checkEscapeBtnClick(event) {
+//   const ESC_KEY_CODE = 'Escape';
+//   const isEscapeKey = event.code === ESC_KEY_CODE;
+//   console.log('відкрито', event.code);
 
-    const bigImageURL = event.target.dataset.source;
-    //console.log(bigImageURL);    
-
-    //Перевірка може бути ще така: event.target.nodeName !==`IMG` => бачила в Репети у вебінарі
-    if (event.currentTarget === event.target) {
-        console.log('Фуууу, мимо');
-        return;
-    }
-    
-    const bigImage = basicLightbox.create(`
-        <img src="${bigImageURL}" width="800" height="600">
-    `)
-        
-    const foo = bigImage.show();
-    console.log('Молодєц, попала');
-};
-
+//   if (isEscapeKey) {
+//     imgShow.close(() => {
+//       window.removeEventListener('keydown', checkEscapeBtnClick);      
+//       console.log('закрито', event.code);
+//     })
+  //   }
+//}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,82 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+/**Сделай такую же галерею как в первом задании, но используя библиотеку SimpleLightbox, 
+ * которая возьмет на себя обработку кликов по изображениям, открытие и закрытие модального окна, 
+ * а также пролистывание изображений при помощи клавиатуры.
+ * 
+ * Необходимо немного изменить разметку карточки галереи, используй этот шаблон.
+
+    <a class="gallery__item" href="large-image.jpg">
+    <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+    </a>
+    
+    Выполняй это задание в файлах 02-lightbox.html и 02-lightbox.js. Разбей его на несколько подзадач:
+    1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. 
+    Используй готовый код из первого задания.
+    2. Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. 
+    Необходимо добавить ссылки на два файла: simple-lightbox.min.js и simple-lightbox.min.css.
+    3. Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery. 
+    Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+    4. Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt. 
+    Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
+*/
+
+const galleryList = document.querySelector('ul.gallery');
+
+const galleryCards = galleryItems.map(createGallery).join('');
+galleryList.insertAdjacentHTML('afterbegin', galleryCards);
+
+function createGallery({ description, preview, original }) { 
+    return `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `
+};
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay:250,});
+// let gallery = new SimpleLightbox('.gallery a');
+
+// gallery.on('show.simplelightbox', function (event) {
+//     event.captionsData: "alt",
+//     event.captionDelay: 250 
+// });
+
+
+
+// SimpleLightbox(captions);
+    // console.log('target', event.target);
+    // console.log('cur.target', event.currentTarget);
+    // const foo = document.querySelectorAll('img.gallery__image');
+    // console.log(foo.alt);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function onGalleryItemClick(event) {
+//     event.preventDefault();
+//     console.log(event.currentTarget);
+//     console.log(event.target);
+
+    
+//     console.log('сімпл лайтбокс', gallery);
+// }
+
+
+
+//console.log('галері ліст', galleryList);
+
+
+
+//console.log('перелік галереї', galleryItems);
